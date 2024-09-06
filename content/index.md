c6ced68b52ef74af7377b1a87d69955e29e70ca7
Joseph Ayala <zelda7009@gmail.com>
2024-09-06 02:24:58 -0500

    t

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: ---
+title: Adalbär's Homebrew Collection
 ---
 
 
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Adalbär's Homebrew Collection",
+    pageTitle: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
